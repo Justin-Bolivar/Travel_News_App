2a076b7f3334d4dc546cbedea9f688866d828829
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 16:40:02 +0800

    travel shar icon added

--- lib/main.dart
+++ lib/main.dart
@@ -164,94 +164,108 @@ class HomeScreen extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemBuilder: (context, index) {
                 return Container(
-                    padding: const EdgeInsets.all(12),
-                    margin: const EdgeInsets.only(
-                      right: 20,
-                    ),
-                    height: 304,
-                    width: 255,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(kBorderRadius),
-                      color: kWhite,
-                      boxShadow: [
-                        BoxShadow(
-                          color: kDarkBlue.withOpacity(0.051),
-                          offset: const Offset(0.0, 3.0),
-                          blurRadius: 24.0,
-                          spreadRadius: 0.0,
+                  padding: const EdgeInsets.all(12),
+                  margin: const EdgeInsets.only(
+                    right: 20,
+                  ),
+                  height: 304,
+                  width: 255,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                    color: kWhite,
+                    boxShadow: [
+                      BoxShadow(
+                        color: kDarkBlue.withOpacity(0.051),
+                        offset: const Offset(0.0, 3.0),
+                        blurRadius: 24.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 164,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(kBorderRadius),
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              'https://a.cdn-hotels.com/gdcs/production143/d1112/c4fedab1-4041-4db5-9245-97439472cf2c.jpg',
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 164,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(kBorderRadius),
-                            image: const DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                'https://a.cdn-hotels.com/gdcs/production143/d1112/c4fedab1-4041-4db5-9245-97439472cf2c.jpg',
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      const Flexible(
+                        child: Text(
+                          'Bali - Uniqure, unmatched. There is no other place like Bali in this world.',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 19,
+                                backgroundColor: kLightBlue,
+                                backgroundImage: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/143471181?v=4',
+                                ),
                               ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 18,
-                        ),
-                        const Flexible(
-                          child: Text(
-                            'Bali - Uniqure, unmatched. There is no other place like Bali in this world.',
-                            style: TextStyle(
-                              fontSize: 15,
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                const CircleAvatar(
-                                  radius: 19,
-                                  backgroundColor: kLightBlue,
-                                  backgroundImage: NetworkImage(
-                                    'https://avatars.githubusercontent.com/u/143471181?v=4',
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 12,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      'Ctrl.',
-                                      style: kPoppinsSemibold.copyWith(
-                                        fontSize: 12,
-                                      ),
+                              const SizedBox(
+                                width: 12,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Ctrl.',
+                                    style: kPoppinsSemibold.copyWith(
+                                      fontSize: 12,
                                     ),
-                                    Text(
-                                      'Sep 9, 2022',
-                                      style: kPoppinsRegular.copyWith(
-                                        color: kGrey,
-                                        fontSize: 12,
-                                      ),
-                                    )
-                                  ],
-                                )
-                              ],
+                                  ),
+                                  Text(
+                                    'Sep 9, 2022',
+                                    style: kPoppinsRegular.copyWith(
+                                      color: kGrey,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Container(
+                            height: 38,
+                            width: 38,
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.circular(kBorderRadius),
+                              color: kLightWhite,
                             ),
-                          ],
-                        ),
-                      ],
-                    ));
+                            child: SvgPicture.asset(
+                              'assets/share_icon.svg',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
               },
             ),
           ),
