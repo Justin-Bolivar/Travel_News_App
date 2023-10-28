d82fe197f96b559ebe6cb4dd8f40f02d3f14556d
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 16:46:48 +0800

    short for you scroll view added

--- lib/main.dart
+++ lib/main.dart
@@ -269,6 +269,119 @@ class HomeScreen extends StatelessWidget {
               },
             ),
           ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Short for You',
+                style: kPoppinsBold.copyWith(
+                  fontSize: 17,
+                ),
+              ),
+              Text(
+                'View all',
+                style: kPoppinsMedium.copyWith(
+                  color: kBlue,
+                  fontSize: 12,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 19,
+          ),
+          SizedBox(
+            height: 88,
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.all(9),
+                  margin: const EdgeInsets.only(
+                    right: 20,
+                  ),
+                  width: 208,
+                  height: 88,
+                  decoration: BoxDecoration(
+                    color: kWhite,
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                    boxShadow: [
+                      BoxShadow(
+                        color: kDarkBlue.withOpacity(0.051),
+                        offset: const Offset(0.0, 3.0),
+                        blurRadius: 24.0,
+                        spreadRadius: 0.0,
+                      )
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 70,
+                        height: 70,
+                        padding: const EdgeInsets.all(26),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(kBorderRadius),
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              'https://homeiswhereyourbagis.com/wp-content/uploads/2019/06/lombok-mustsees-mawun-beach-drohne.jpg',
+                            ),
+                          ),
+                        ),
+                        child: SvgPicture.asset(
+                          'assets/play_icon.svg',
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      Flexible(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Top Trending Island in 2022',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: kPoppinsSemibold.copyWith(
+                                fontSize: 13,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            Row(
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/eye_icon.svg',
+                                ),
+                                const SizedBox(
+                                  width: 4,
+                                ),
+                                Text(
+                                  '40,999',
+                                  style: kPoppinsMedium.copyWith(
+                                    color: kGrey,
+                                    fontSize: 12,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
         ],
       ),
     );
