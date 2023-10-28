e6c3ceaec658023c74c566b0416d247282d49803
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 15:59:11 +0800

    user info finished

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,60 @@ class _MyWidgetState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: kLighterWhite,
+      home: Scaffold(backgroundColor: kLighterWhite, body: HomeScreen()),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+        ),
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 51,
+                width: 51,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(kBorderRadius),
+                  color: kLightBlue,
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                      'https://cdn3d.iconscout.com/3d/premium/thumb/man-avatar-6299539-5187871.png',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome Back!',
+                    style: kPoppinsBold.copyWith(fontSize: 16),
+                  ),
+                  Text(
+                    'Monday, 31 October',
+                    style: kPoppinsRegular.copyWith(color: kGrey, fontSize: 12),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
